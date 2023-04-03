bc990153203508e259546e418f32da18ca5c7cc0
Damkols <damolausmankolapo@gmail.com>
2023-04-03 14:33:49 +0100

    create project and installed Chakra UI

--- README.md
+++
@@ -1,1 +0,0 @@
-# rawg-clone
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+const App = () => {
+ return (
+  <div>
+   <Button colorScheme="blue">Button</Button>
+  </div>
+ );
+};
+
+export default App;
