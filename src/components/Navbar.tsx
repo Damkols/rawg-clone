9cc2483c3a6bc8292f4c928b892c2fe2d7c7126c
Damkols <damolausmankolapo@gmail.com>
2023-04-13 11:37:40 +0100

    feat: implement game search feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,16 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const Navbar = () => {
+interface SearchInputProps {
+ onSearch: (searchText: string) => void;
+}
+
+const Navbar = ({ onSearch }: SearchInputProps) => {
  return (
   <div>
    <HStack padding="10px">
     <Image src={logo} boxSize="60px" />
-    <SearchInput />
+    <SearchInput onSearch={onSearch} />
     <ColorModeSwitch />
    </HStack>
   </div>
