17f510a3e0a2a1b62113f07f58978b3146a4fb1f
Damkols <damolausmankolapo@gmail.com>
2023-04-17 11:40:29 +0100

    feat: cleaning up the game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,13 +12,13 @@ const GameCard = ({ game }: GameProps) => {
   <Card>
    <Image src={getCroppedImageUrl(game.background_image)} />
    <CardBody>
-    <Heading fontSize="1xl">{game.name}</Heading>
-    <HStack justifyContent="space-between" paddingY="15px">
+    <HStack justifyContent="space-between" paddingY="5px" marginBottom={3}>
      <PlatformIconList
       platforms={game.parent_platforms.map((p) => p.platform)}
      />
      <CriticScore score={game.metacritic} />
     </HStack>
+    <Heading fontSize="1xl">{game.name}</Heading>
    </CardBody>
   </Card>
  );
